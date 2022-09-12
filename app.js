f27d9aa96845cc03eae419962f02ad0608ee5962
Sam Adams <sam.adams7785@gmail.com>
2022-09-12 16:38:57 -0700

    created display shells function and set game state and started added logic to show basic view on game state

--- app.js
+++ app.js
@@ -2,15 +2,38 @@
 // import { getRandomItem } from './utils.js';
 
 /* State */
+let gameState = 'guess';
 
 /* Actions */
-function loadPage() {}
-
+function loadPage() {
+    displayShells();
+}
 /* Components */
+/* scoreboard */
 
-/* Component */
+/* guess */
+
 // get DOM
+const guessShell1 = document.getElementById('shell-1');
+const guessShell2 = document.getElementById('shell-2');
+const guessShell3 = document.getElementById('shell-3');
 // display
+
+// function displayGuess() {
+//     guessShell1.classList.remove('win', 'lose');
+//     guessShell2.classList.remove('win', 'lose');
+//     guessShell3.classList.remove('win', 'lose');
+// }
+
+//DOM
+const results = document.getElementById('results');
+
+//display
+function displayShells() {
+    if (gameState === 'guess') {
+        results.classList.add('hidden');
+    }
+}
 // event listeners
 
 /* Run page load code */
