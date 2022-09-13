40eabe517f26de6771b6dfa1e57fd0cae72666f2
Sam Adams <sam.adams7785@gmail.com>
2022-09-12 22:46:35 -0700

    Add Guess Click Event Listeners

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* Imports */
-// import { getRandomItem } from './utils.js';
+import { getRandomItem } from './utils.js';
 
 /* State */
 let gameState = 'guess';
@@ -7,8 +7,28 @@ let guess = '';
 let reveal = '';
 let result = '';
 
-let total = 2;
-let wins = 1;
+let total = 0;
+let wins = 0;
+
+const results = document.getElementById('results');
+
+const pearl1 = document.getElementById('pearl-1');
+const pearl2 = document.getElementById('pearl-2');
+const pearl3 = document.getElementById('pearl-3');
+
+const shell1 = document.getElementById('shell-1');
+const shell2 = document.getElementById('shell-2');
+const shell3 = document.getElementById('shell-3');
+
+const totalDisplay = document.getElementById('total-display');
+const winsDisplay = document.getElementById('wins-display');
+const lossesDisplay = document.getElementById('losses-display');
+
+const guessOne = document.getElementById('guess-1');
+const guessTwo = document.getElementById('guess-2');
+const guessThree = document.getElementById('guess-3');
+
+const guesses = ['shell1', 'shell2', 'shell3'];
 
 /* Actions */
 function loadPage() {
@@ -19,10 +39,6 @@ function loadPage() {
 
 /* scoreboard */
 
-const totalDisplay = document.getElementById('total-display');
-const winsDisplay = document.getElementById('wins-display');
-const lossesDisplay = document.getElementById('losses-display');
-
 function displayScoreboard() {
     totalDisplay.textContent = total;
     winsDisplay.textContent = wins;
@@ -30,21 +46,27 @@ function displayScoreboard() {
 }
 
 /* guess */
+function randomPearl(userGuess) {
+    gameState = 'results';
+    guess = userGuess;
+    const guessShell = getRandomItem(guesses);
+    total++;
+
+    if (guess === reveal) {
+        result = 'win';
+        wins++;
+    } else {
+        result = 'lose';
+    }
+
+    loadPage();
+}
 
 // get DOM
 
 // display
 
 //DOM
-const results = document.getElementById('results');
-
-const pearl1 = document.getElementById('pearl-1');
-const pearl2 = document.getElementById('pearl-2');
-const pearl3 = document.getElementById('pearl-3');
-
-const shell1 = document.getElementById('shell-1');
-const shell2 = document.getElementById('shell-2');
-const shell3 = document.getElementById('shell-3');
 
 //display
 function displayShells() {
@@ -56,9 +78,21 @@ function displayShells() {
         shell1.classList.remove('reveal');
     } else {
         results.classList.remove('hidden');
+        pearl1.classList.remove('hidden');
     }
 }
+
 // event listeners
+
+guessOne.addEventListener('click', () => {
+    randomPearl('shell-1');
+});
+guessTwo.addEventListener('click', () => {
+    randomPearl('shell-2');
+});
+guessThree.addEventListener('click', () => {
+    randomPearl('shell-3');
+});
 
 /* Run page load code */
 loadPage();
