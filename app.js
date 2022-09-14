bb3757d1380a4969625a38c081883d8cf65d1208
Sam Adams <sam.adams7785@gmail.com>
2022-09-14 15:47:57 -0700

    correctly reveals pearl/shells and hides guess on result

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ const guessOne = document.getElementById('guess-1');
 const guessTwo = document.getElementById('guess-2');
 const guessThree = document.getElementById('guess-3');
 
+const guessesBox = document.getElementById('guesses');
+
 const guesses = ['shell1', 'shell2', 'shell3'];
 
 /* Actions */
@@ -48,19 +50,39 @@ function displayScoreboard() {
 /* guess */
 function randomPearl(userGuess) {
     gameState = 'results';
-    guess = userGuess;
-    const guessShell = getRandomItem(guesses);
-    console.log(guessShell);
-    console.log(userGuess);
+    guessesBox.classList.add('hidden');
+    const randomPearl = getRandomItem(guesses);
     total++;
 
-    if (userGuess === guessShell) {
-        result = 'win';
+    if ('shell1' === randomPearl) {
+        shell1.classList.add('reveal');
+        pearl1.classList.remove('hidden');
+    }
+    if ('shell2' === randomPearl) {
+        shell2.classList.add('reveal');
+        pearl2.classList.remove('hidden');
+    }
+    if ('shell3' === randomPearl) {
+        shell3.classList.add('reveal');
+        pearl3.classList.remove('hidden');
+    }
+
+    const display1 = document.getElementById('display-1');
+    const display2 = document.getElementById('display-2');
+    const display3 = document.getElementById('display-3');
+
+    if (userGuess === randomPearl) {
+        result = 'win'; // change text to Found it
         wins++;
     } else {
         result = 'lose';
+        // change text to not here
     }
-    console.log(result);
+
+    console.log(userGuess, 'guess');
+
+    console.log(randomPearl, 'computer');
+
     loadPage();
 }
 
@@ -74,13 +96,15 @@ function randomPearl(userGuess) {
 function displayShells() {
     if (gameState === 'guess') {
         results.classList.add('hidden');
+        guessesBox.classList.remove('hidden');
         pearl1.classList.add('hidden');
         pearl2.classList.add('hidden');
         pearl3.classList.add('hidden');
         shell1.classList.remove('reveal');
+        shell2.classList.remove('reveal');
+        shell3.classList.remove('reveal');
     } else {
         results.classList.remove('hidden');
-        pearl1.classList.remove('hidden');
     }
 }
 
@@ -88,19 +112,21 @@ function displayShells() {
 
 guessOne.addEventListener('click', () => {
     randomPearl('shell1');
+    shell1.classList.add('reveal');
 });
 guessTwo.addEventListener('click', () => {
     randomPearl('shell2');
+    shell2.classList.add('reveal');
 });
 guessThree.addEventListener('click', () => {
     randomPearl('shell3');
+    shell3.classList.add('reveal');
 });
 
 const playAgainButton = document.getElementById('play-again-button');
 
 playAgainButton.addEventListener('click', () => {
     playAgain();
-    console.log('play again');
 });
 
 function playAgain() {
